808a8d7e84fdb786fa5066fa667499646e751844
NakajimaShinya <nakajima.shinya11@gmail.com>
2017-10-22 23:57:44 +0900

    add and done task

--- app/javascript/packs/components/index.vue
+++ app/javascript/packs/components/index.vue
@@ -3,10 +3,10 @@
         <!-- 新規作成部分 -->
         <div class="row">
             <div class="col s10 m11">
-                <input class="form-control" placeholder="Add your task!!">
+                <input v-model="newTask" class="form-control" placeholder="Add your task!!">
             </div>
             <div class="col s2 m1">
-                <div class="btn-floating waves-effect waves-light red">
+                <div v-on:click="createTask" class="btn-floating waves-effect waves-light red">
                     <i class="material-icons">add</i>
                 </div>
             </div>
@@ -15,7 +15,7 @@
         <div>
             <ul class="collection">
                 <li v-for="task in tasks" v-if="!task.done" v-bind:id="'row_task_' + task.id" class="collection-item">
-                    <input type="checkbox" v-bind:id="'task_' + task.id" />
+                    <input v-on:change="doneTask(task.id)" type="checkbox" v-bind:id="'task_' + task.id" />
                     <label v-bind:for="'task_' + task.id">{{task.name}}</label>
                 </li>
             </ul>
@@ -63,6 +63,33 @@
             },
             displayFinishedTasks: function() {
                 document.querySelector('#finished-tasks').classList.toggle('display_none');
+            },
+            createTask: function (){
+                if (!this.newTask) return;
+
+                axios.post('/api/tasks', { task: {name: this.newTask}}).then((response) => {
+                    this.tasks.unshift(response.data.task);
+                    this.newTask = '';
+                }), (error) => {
+                    console.log(error);
+                }
+            },
+            doneTask: function (id) {
+                axios.put('/api/tasks/' + id, { task: { done: 1 }}).then((response) => {
+                    this.moveFinishedTask(id);
+                }), (error) => {
+                    console.log(error);
+                }
+            },
+            moveFinishedTask: function (id) {
+                var el = document.querySelector('#row_task_' + id);
+                var el_clone = el.cloneNode(true);
+                el.classList.add('display_none');
+                el_clone.getElementsByTagName('input')[0].checked = 'checked';
+                el_clone.getElementsByTagName('label')[0].classList.add('line-through');
+                el_clone.getElementsByTagName('label')[0].classList.remove('word-color-black');
+                var li = document.querySelector('#finished-tasks > ul > li:first-child');
+                document.querySelector('#finished-tasks > ul').insertBefore(el_clone, li);
             }
         }
     }
