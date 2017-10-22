d6ed9bfc5f366806c1411a2640dd13e744907854
NakajimaShinya <nakajima.shinya11@gmail.com>
2017-10-22 23:00:36 +0900

    setting routes

---
+++ app/javascript/packs/todo.js
@@ -0,0 +1,11 @@
+import Vue from 'vue/dist/vue.esm';
+import Router from './router';
+import Header from './components/header.vue';
+
+var app = new Vue({
+    router: Router,
+    el: '#app',
+    components: {
+        'navbar': Header,
+    }
+});
